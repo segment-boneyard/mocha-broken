e24c6376cc96102d861138434bfdba2b826187d5
Julian Gruber <julian@juliangruber.com>
2014-04-05 18:05:31 -0700

    proof of concept

---
+++ index.js
@@ -0,0 +1,41 @@
+var spawn = require('child_process').spawn;
+var split = require('split');
+var fs = require('fs');
+var quote = require('quotemeta');
+var mocha = require('mocha');
+
+var broken = {};
+var bin = __dirname + '/node_modules/.bin/mocha';
+var filename = process.cwd() + '/.mocha-broken';
+
+try {
+  broken = JSON.parse(fs.readFileSync(filename, 'utf8'));
+} catch (_) {}
+
+var args = ['--reporter', 'json-stream'];
+if (Object.keys(broken).length) {
+  args.push('--grep');
+  args.push(Object.keys(broken).map(quote).join('|'));
+}
+
+var ps = spawn(bin, args);
+ps.stdout.pipe(split()).on('data', function(line){
+  console.log(line);
+
+  var update = JSON.parse(line);
+  var type = update[0];
+  var test = update[1];
+  var title = test.title;
+
+  if ('start' == type) return;
+  if ('pass' == type) return delete broken[title];
+  if ('broken' == type) return broken[title] = true;
+  if ('end' == type) {
+    fs.writeFileSync(filename, JSON.stringify(broken));
+  }
+});
+ps.stderr.pipe(process.stderr);
+ps.on('exit', function(code){
+  process.exit(code);
+});
+
---
+++ test.js
@@ -0,0 +1,8 @@
+describe('suite', function(){
+  it('should break', function(){
+    throw new Error('broke');
+  })
+  it('should pass', function(){
+    // yup
+  });
+});
